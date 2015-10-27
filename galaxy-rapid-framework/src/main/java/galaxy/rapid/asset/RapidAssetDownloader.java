2dd2d60bedb0aecc1a3d7d7006105613a207be52
Radomiej <radoslawbojba.programista@gmail.com>
2015-10-27 21:27:52 +0100

    Add initial download manager.

--- galaxy-rapid-framework/src/main/java/galaxy/rapid/asset/RapidAssetDownloader.java
+++ galaxy-rapid-framework/src/main/java/galaxy/rapid/asset/RapidAssetDownloader.java
@@ -1,14 +1,16 @@
-package galaxy.rapid.downloader;
+package galaxy.rapid.asset;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-import org.junit.Test;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 
+import galaxy.radpid.configuration.RapidConfiguration;
 import galaxy.rapid.downloader.AsynAdvenceDownloader;
 import galaxy.rapid.downloader.DownloadInfo;
 import galaxy.rapid.downloader.DownloadListener;
@@ -16,14 +18,13 @@ import galaxy.rapid.downloader.DownloadUtils;
 import galaxy.rapid.downloader.exceptions.CanNotStartDownload;
 import galaxy.rapid.unzip.Unzipp;
 
-public class DownloadTest {
-
-	@Test
-	public void test() {
-		
+public enum RapidAssetDownloader {
+	INSTANCE;
+	
+	public void loadAsset(String urlString) {
 		URL url = null;
 		try {
-			url = new URL("http://www.helloschool.com.pl/uploads/8/3/1/5/8315217/unit13.zip");
+			url = new URL(urlString);
 		} catch (MalformedURLException e1) {
 			e1.printStackTrace();
 			fail("B³ad utworzenia adresu URL");
@@ -42,14 +43,14 @@ public class DownloadTest {
 			@Override
 			public void downloadComplete(File tempFile, DownloadInfo downloadInfo) {
 				System.out.println("download complete");
-				File outputZipFolder = new File("outputZip");
-				
+				FileHandle assetFolder = Gdx.files.external(RapidConfiguration.INSTANCE.getAppName());
+				File file = assetFolder.file();
+				System.out.println("Unzip do: " + file.getAbsolutePath());
 				Unzipp unzipp = new Unzipp();
-				unzipp.unzip(tempFile, "outputZip");
+				unzipp.unzip(tempFile, file.getAbsolutePath());
 				
 				System.out.println("unzip complete");
-				assertTrue(outputZipFolder.delete());
-				assertTrue(tempFile.delete());
+				tempFile.delete();
 			}
 		});
 		
@@ -57,12 +58,10 @@ public class DownloadTest {
 			downloader.startDownload();
 		} catch (CanNotStartDownload e) {
 			e.printStackTrace();
-			fail("B³ad pobierania");
 		}
 		
 		while(!downloader.isComplete()){
 			
 		}
 	}
-
 }
